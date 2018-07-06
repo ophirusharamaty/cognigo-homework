e8f9eaf64a5e6416a5b1ce4611a5dc55e094ce17
Ophir <dreckguy@gmail.com>
2018-07-06 10:20:44 +0000

    add redis http client for uptime robot

--- health.sh
+++ health.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #elasticsearch healthcheck
-curl localhost:9200/_cat/health
+#curl localhost:9200/_cat/health
 #redis healthcheck use info for monitoring()
-docker exec -it cognigohomework_redis_1 redis-cli ping
+#docker exec -it cognigohomework_redis_1 redis-cli ping
+
+
+#using tests in docker
+docker inspect --format='{{json .State.Health}}' $(docker ps -q)
