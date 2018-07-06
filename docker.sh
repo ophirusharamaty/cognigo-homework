36e31a69ceb1e4bbac303fa397f41b9e3f990f8e
Ophir <dreckguy@gmail.com>
2018-07-06 02:45:59 +0000

    initial commit

---
+++ docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get -y install \
+  apt-transport-https \
+  ca-certificates \
+  curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+sudo usermod -aG docker ${USER}
+
+sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose -v
