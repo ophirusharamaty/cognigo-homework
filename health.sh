38e95f48b66cb526a5731269cc007ac407fd212c
Ophir <dreckguy@gmail.com>
2018-07-06 08:15:58 +0000

    add healthcheck to both services

--- health.sh
+++ health.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #elasticsearch healthcheck
 curl localhost:9200/_cat/health
-#redis healthcheck
+#redis healthcheck use info for monitoring()
 docker exec -it cognigohomework_redis_1 redis-cli ping
